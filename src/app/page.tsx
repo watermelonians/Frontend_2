4ab59e73956ce5c1d39a22fc5f427163e59d6fe3
sohaib20030720 <sohaib.mousselmal@ensia.edu.dz>
2024-04-21 15:57:14 +0100

    define routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React, { useState, ReactNode } from "react";
-import RatioRepresentation from "@/components/Overview/RatioRepresentation";
-import ECommerce from "@/components/Overview/Overview";
+import Overview from "./Overview/Overview";
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <ECommerce/>    
+      <Overview/>    
     </DefaultLayout>
   );
 }

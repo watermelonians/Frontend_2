86e1de3f8d22d70e2c4b448aa15913c2e6a9f2cd
maabChaoui <maab.chaoui@ensia.edu.dz>
2024-04-29 17:49:21 +0100

    firebase re-config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,8 @@ import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
 
-import { initializeApp } from "firebase/app";
-
-import {
-  initializeAuth,
-  indexedDBLocalPersistence,
-  inMemoryPersistence,
-} from "firebase/auth";
-
-import { FirebaseAppProvider, AuthProvider } from "reactfire";
-
-import configuration from "../../configuration";
-import { isBrowser } from "../../lib/generic/isBrowser";
+// import "../../config/firebase.config";
+import { AuthContextProvider } from "../context/AuthContext";
 
 export default function RootLayout({
   children,
@@ -37,28 +27,16 @@ export default function RootLayout({
   // console.log("pls work");
 
   // we initialize the firebase app
-  // using the configuration that we defined above
-  const app = initializeApp(configuration.firebase);
-
-  // make sure we're not using IndexedDB when SSR
-  // as it is only supported on browser environments
-  const persistence = isBrowser()
-    ? indexedDBLocalPersistence
-    : inMemoryPersistence;
-
-  const auth = initializeAuth(app, { persistence });
 
   return (
-    <FirebaseAppProvider firebaseApp={app}>
-      <AuthProvider sdk={auth}>
-        <html lang="en">
-          <body suppressHydrationWarning={true}>
-            <div className="dark:bg-boxdark-2 dark:text-bodydark">
-              {loading ? <Loader /> : children}
-            </div>
-          </body>
-        </html>
-      </AuthProvider>
-    </FirebaseAppProvider>
+    <AuthContextProvider>
+      <html lang="en">
+        <body suppressHydrationWarning={true}>
+          <div className="dark:bg-boxdark-2 dark:text-bodydark">
+            {loading ? <Loader /> : children}
+          </div>
+        </body>
+      </html>
+    </AuthContextProvider>
   );
 }

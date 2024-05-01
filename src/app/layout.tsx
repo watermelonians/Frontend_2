679742c9331a4f92fe769fcd0e3175a2a695bdf1
Ahmed Djalal HACINI <ahmed.hacini@ensia.edu.dz>
2024-05-01 19:01:50 +0100

    "Code fixes + adding loading and not-found"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
+import React from "react";
 
 // import "../../config/firebase.config";
 import { AuthContextProvider } from "../context/AuthContext";
@@ -15,27 +14,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  // const pathname = usePathname();
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 0);
-    // setTimeout(() => setLoading(false), 1000);
-  }, []);
-
-  // console.log("pls work");
-
-  // we initialize the firebase app
-
   return (
     <AuthContextProvider>
       <html lang="en">
         <body suppressHydrationWarning={true}>
           <DefaultLayout>
             <div className="dark:bg-boxdark-2 dark:text-bodydark">
-              {loading ? <Loader /> : children}
+              {children}
             </div>
           </DefaultLayout>
         </body>

23dbf57be3515bf9c94bc0b83a2641f969be4b6c
Ahmed Djalal HACINI <ahmed.hacini@ensia.edu.dz>
2024-05-01 19:04:43 +0100

    "next-config config"

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "file-example.com",
+        port: "",
+        pathname: "/**",
+      }
     ],
   },
 };
